3f2dafaf8a9114c34cfcf74c42d3a7f982326499
Marc <marcduchb20@gmail.com>
2023-05-31 01:38:20 +0200

    Refactor Tiles + Debug.Log updates

--- Assets/Scripts/Tile/TriggerComponent.cs
+++ Assets/Scripts/Tile/TriggerComponent.cs
@@ -4,27 +4,29 @@ using UnityEngine;
 public class TriggerComponent : MonoBehaviour {
     [SerializeField]
     BaseTile tile;
+    bool debug = false;
 
-    public void init(BaseTile tile){
+    public void init(BaseTile tile, bool debug = false){
         this.tile = tile;
+        this.debug = debug;
     }
     
     private void OnTriggerEnter(Collider other) {
         EntityController entity = other.GetComponent<EntityController>();
-        if (entity == null) Debug.LogError("[TriggerComponent] No entity controller found");
+        if (entity == null) Debug.LogError("[TriggerComponent] No entity controller found", this);
         else if (tile != null) {
-            Debug.Log("[TriggerComponent] " + tile);
+            if (debug) Debug.Log("[TriggerComponent] " + tile.transform.parent, this);
             tile.onTriggerEnter(entity);
-        } else Debug.LogError("[TriggerComponent] Tile is null");    
+        } else Debug.LogError("[TriggerComponent] Tile is null", this);    
     }
 
     private void OnTriggerExit(Collider other) {
         EntityController entity = other.GetComponent<EntityController>();
-        if (entity == null) Debug.LogError("[TriggerComponent] No entity controller found");
+        if (entity == null) Debug.LogError("[TriggerComponent] No entity controller found", this);
         else if (tile != null) {
-            Debug.Log("[TriggerComponent] " + tile);
+            if (debug) Debug.Log("[TriggerComponent] " + tile.transform.parent, this);
             tile.onTriggerExit(entity);
-        } else Debug.LogError("[TriggerComponent] Tile is null");
+        } else Debug.LogError("[TriggerComponent] Tile is null", this);
     }
 
 }
