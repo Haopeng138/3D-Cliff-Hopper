2f0df2b5983aa7bee5bef35bf4f0fbecf9eeec29
Marc <marcduchb20@gmail.com>
2023-05-31 01:36:30 +0200

    Refactor TileSpawning
    Tile Weights now determines whether a tile can be replaced or not
    Rotation Tiles are selected on BaseTileSO nextTile()

--- Assets/Scripts/Tile/Controller/TileMapController.cs
+++ Assets/Scripts/Tile/Controller/TileMapController.cs
@@ -2,56 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-    public enum Direction { Z=0, X=1 };
+    public enum Direction { Z = +1, X = -1 };
 
 
 public class TileMapController : MonoBehaviour
 {
+    public bool debug = false;
     public int maxTiles;
-    public Vector3 tileLocation;
-    public BaseTileSO currentTile;
+    public Vector3 tileSpawnLocation;
 
+    [Header("Tile Types")]
+    public BaseTileSO currentTile;
     public BaseTileSO rotateTile;
+    public BaseTileSO defaultTile;
+
+    // Direction of the next tile
     Direction currentDirection = Direction.Z;
 
+// Offset from center of the map
+    // Current center offset from (0,0)
     private int centerOffset = 0;
     public int centerOffsetMin = 2;
     public int centerOffsetMax = 4;
+    public bool ReachedMaxOffset { get {return centerOffset * (int)currentDirection > centerOffsetMax; } }
 
-    public int currentHeight = 0;
+
+// HEIGHT
+    private int currentHeight = 0;
     public int maxHeight = 10;
     public int minHeight = -3;
 
 
+    public bool ReachedMaxHeight { get { return currentHeight >= maxHeight; } }
+    public bool ReachedMinHeight { get { return currentHeight <= minHeight; } }
+
     // Start is called before the first frame update
     void Start()
     { 
-        spawnCurrentTile();
+        Vector3 tileSpawnLocation = transform.position;
+        centerOffset = (int)(tileSpawnLocation.z - tileSpawnLocation.x);
+        currentDirection = centerOffset > 0 ? Direction.X : Direction.Z;
+                
         CreateMap();        
     }
 
 
     void CreateMap(){
+        
+        if (debug) {
+            Debug.Log("CREATE MAP");
+            Debug.Log("Initial Spawn Location: " + tileSpawnLocation);
+            Debug.Log("Initial Tile: " + currentTile);
+            Debug.Log("Initial Direction: " + currentDirection);
+            Debug.Log("Initial CenterOffset: " + centerOffset);
+        }
+        // Spawn first tile
+        spawnCurrentTile();
+
         for (int i = 0; i < maxTiles; i++)
         {
-
-            nextTile();
-            
-            centerOffset += currentDirection == Direction.X ? -1 : 1;
-            
-            if (currentTile.canBeReplaced)
-            {
-                switch (currentDirection)
-                {
-                    case Direction.X:
-                        if (Random.Range(0, -centerOffsetMax -1) > centerOffset && centerOffset < -centerOffsetMin) changeDirection();
-                    break;
-                    case Direction.Z:
-                    if (Random.Range(0, centerOffsetMax +1) < centerOffset && centerOffset > centerOffsetMin) changeDirection();
-                    break;                    
-                }
-            }
-            
+            nextTile();    
+            centerOffset += (int)currentDirection;
             spawnCurrentTile();
         }
     }
@@ -59,12 +70,15 @@ public class TileMapController : MonoBehaviour
     void nextTile(){
         addStride(false);
         currentTile = currentTile.nextTile(this);
-        addStride(true);
+        if (currentTile == rotateTile){
+            if(debug) Debug.Log("Rotate Tile", currentTile);
+        } 
+        else addStride(true);
     
     }
 
     BaseTile spawnCurrentTile(){
-        BaseTile newTile = currentTile.spawnTile(tileLocation, currentDirection, this);
+        BaseTile newTile = currentTile.spawnTile(tileSpawnLocation, currentDirection, this);
         newTile.offset = centerOffset;
         newTile.gameObject.transform.parent.transform.parent = gameObject.transform;
         return newTile;
@@ -74,19 +88,23 @@ public class TileMapController : MonoBehaviour
         switch(currentDirection) 
         {
         case Direction.Z:
-            tileLocation.z += currentTile.nextTileStride.x;
+            tileSpawnLocation.z += currentTile.nextTileStride.x;
         break;
         case Direction.X:
-            tileLocation.x += currentTile.nextTileStride.x;
+            tileSpawnLocation.x += currentTile.nextTileStride.x;
         break;
         }
+
         if (height) {
-            tileLocation.y += currentTile.nextTileStride.y;
+            tileSpawnLocation.y += currentTile.nextTileStride.y;
             currentHeight += (int)currentTile.nextTileStride.y;
         }
     }
 
-    void changeDirection(){
+    // Changes the direction of the next tile in the controller 
+    // Returns a tile that allows Rotation the tile to be rotated
+    public BaseTileSO changeDirection(){
+        addStride(false);
         switch(currentDirection) 
         {
         case Direction.Z:
@@ -96,16 +114,26 @@ public class TileMapController : MonoBehaviour
             currentDirection = Direction.Z;
         break;
         }
-        currentTile = rotateTile;
+        return rotateTile;
     }
 
-    public bool reachedMaxOffset(){
-        if (centerOffset <  -centerOffsetMax && currentDirection == Direction.X) {
-            return true;
+    // Roll for "rotateTile" in relation to centerOffset
+    // Return -> TRUE: Rotate, FALSE: Don't Rotate
+    public bool rollRotateTileByDistance(){
+        //if (!currentTile.canBeReplaced) return false; 
+        if (ReachedMaxOffset) return true;
+
+        bool roll = false;
+
+        if (currentDirection == Direction.X) {
+            roll = centerOffset < Random.Range(-centerOffsetMax, -centerOffsetMin);
+        } else {
+            roll = centerOffset > Random.Range(centerOffsetMin, centerOffsetMax) ;
+
         }
-        if (centerOffset >  centerOffsetMax && currentDirection == Direction.Z) {
-            return true;
-        }
-        return false;
+
+        return roll;
     }
+
+
 }
